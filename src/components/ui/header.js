7268915ba7ff52e303d470ac6b985bc38c133085
d3v3lop3r1 <d3v3lop3r1@gmail.com>
2024-10-07 20:53:35 +0200

    show active nav link

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,9 +1,13 @@
+'use client'
+import { usePathname } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 import { Anchor } from "lucide-react"
 import Link from "next/link"
 import "@/app/globals.css"
 
 export default function Header(){
+    const pathname = usePathname()
+    console.log(pathname)
     return (
     <header className="header px-4 lg:px-6 h-20 flex items-center border-b">
         <Link className="flex items-center justify-center" href="#">
@@ -11,22 +15,22 @@ export default function Header(){
           <span className="ml-2 text-2xl font-bold text-primary">Zlatna Luka</span>
         </Link>
         <nav className="ml-auto flex items-center gap-4 sm:gap-6">
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/">
+          <Link className={`${pathname === '/' ? 'active' : ''} text-sm font-medium hover:underline underline-offset-4`} href="/">
             Home
           </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/yachts">
+          <Link className={`${pathname === '/yachts' ? 'active' : ''} text-sm font-medium hover:underline underline-offset-4`} href="/yachts">
             Yacht Fleet
           </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/destinations">
+          <Link className={`${pathname === '/destinations' ? 'active' : ''} text-sm font-medium hover:underline underline-offset-4`} href="/destinations">
             Destinations
           </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/charters">
+          <Link className={`${pathname === '/charters' ? 'active' : ''} text-sm font-medium hover:underline underline-offset-4`} href="/charters">
             Charter Packages
           </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/about-us">
+          <Link className={`${pathname === '/about-us' ? 'active' : ''} text-sm font-medium hover:underline underline-offset-4`} href="/about-us">
             About Us
           </Link>
-          <Link className="text-sm font-medium hover:underline underline-offset-4" href="/contact">
+          <Link className={`${pathname === '/contact' ? 'active' : ''} text-sm font-medium hover:underline underline-offset-4`} href="/contact">
             Contact
           </Link>
           <Button>Book Now</Button>
